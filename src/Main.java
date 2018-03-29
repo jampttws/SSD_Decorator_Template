08c35a3f49d218671b80aeb1c24cf71d6a29efb7
Keeratipong Ukachoke <kee@glazziq.com>
2018-03-29 11:00:30 +0700

    Inititial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ImageObject img1 = new ImageObject(80, 100);
+        Window window = new Window(img1);
+        window.setVisible(true);
+    }
+
+}
